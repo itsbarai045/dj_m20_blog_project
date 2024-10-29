6fec3390194561680b593044b89e8e4c450405e9
barai3045 <santosh.barai.045@gmail.com>
2024-10-30 00:09:34 +0600

    init

--- blog_project/views.py
+++ blog_project/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render
 from posts.models import Post
 
 def home(request):
-    data = Post.objects.all()
+    query = request.GET.get('search')
+    if query:
+        data = Post.objects.filter(title__icontains=query) | Post.objects.filter(category__name__icontains=query)
+        data = data.distinct()
+    else:
+        data = Post.objects.all()
     
     return render(request, 'home.html', {'data': data})
